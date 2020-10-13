a1035f1fc8a5cfff03daeca85a453c8e9e7dc090
nguyenthithuuyen <nuyen2054@gmail.com>
2020-10-13 10:08:27 +0700

    nop

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "xin chao"
+?>
+<?php date_default_timezone_set('Asia/Ho_Chi_Minh') ?>
+
+<html>
+<head>
+    <title>Thời gian hiện tại</title>
+</head>
+<body>
+<h1> Bây giờ là: <?php echo date('Y-M-d h:m:s'); ?> </h1>
+</body>
+</html>
